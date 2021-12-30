e6e89cc2ec4022136c839edcff3362e8aba3f347
Tom Fletcher <thomasmatthewfletcher@gmail.com>
2021-12-30 23:22:51 +0000

    Complete day 02-1

---
+++ 02/day_02_1.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+from typing import List
+from enum import IntEnum
+
+Program = List[int]
+
+class OpCode(IntEnum):
+    ADD = 1
+    MULTIPLY = 2
+    HALT = 99
+
+
+class Intcode:
+    def __init__(self, program: Program):
+        self.program = program
+        self.current = 0
+        self.halted = False
+
+        self.instructions = {
+            OpCode.ADD: self.instruction_add,
+            OpCode.MULTIPLY: self.instruction_multiply,
+            OpCode.HALT: self.instruction_halt
+        }
+
+    def run(self):
+        while not self.halted:
+            self.run_instruction()
+
+    def run_instruction(self):
+        opcode = self.read_opcode()
+
+        self.instructions[opcode]()
+
+    def read(self) -> int:
+        value = self.program[self.current]
+        self.current += 1
+        return value
+
+    def read_opcode(self) -> OpCode:
+        return OpCode(self.read())
+
+    def read_at(self, index: int):
+        return self.program[index]
+
+    def instruction_add(self):
+        input_index_1 = self.read()
+        input_index_2 = self.read()
+        output_index = self.read()
+
+        input_1 = self.read_at(input_index_1)
+        input_2 = self.read_at(input_index_2)
+
+        self.program[output_index] = input_1 + input_2
+
+    def instruction_multiply(self):
+        input_index_1 = self.read()
+        input_index_2 = self.read()
+        output_index = self.read()
+
+        input_1 = self.read_at(input_index_1)
+        input_2 = self.read_at(input_index_2)
+
+        self.program[output_index] = input_1 * input_2
+
+    def instruction_halt(self):
+        self.halted = True
+
+
+def main():
+    program = read_program()
+    program[1] = 12
+    program[2] = 2
+    intcode = Intcode(program)
+    intcode.run()
+    value = intcode.read_at(0)
+    print(f'Value at position 0: {value}')
+
+def read_program() -> Program:
+    with open('input.txt') as f:
+        return [int(v) for v in f.readline().split(',')]
+
+if __name__ == '__main__':
+    main()
