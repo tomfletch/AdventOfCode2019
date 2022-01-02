875a45e1faaffc9e60fa612f29819c67a2da3ca6
Tom Fletcher <thomasmatthewfletcher@gmail.com>
2022-01-02 02:10:50 +0000

    Complete day 09-2

---
+++ 09/day_09_2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from Intcode import Computer, read_program
+
+def main():
+    program = read_program()
+    computer = Computer(program)
+    computer.run([2])
+
+
+if __name__ == '__main__':
+    main()
